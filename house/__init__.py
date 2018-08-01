43a71d21b27c5eb24c239e205cb7faa41ec8645f
Erin Carvalho <emc@cs50.harvard.edu>
2018-08-01 11:50:15 -0400

    adds checks for ap specifc problems

---
+++ house/__init__.py
@@ -0,0 +1,8 @@
+import check50
+
+# Coming soon...
+
+@check50.check()
+def submitted():
+    """Around the House submitted"""
+    pass
---
+++ teacher/__init__.py
@@ -0,0 +1,8 @@
+import check50
+
+# Coming soon...
+
+@check50.check()
+def submitted():
+    """Be the Teacher submitted"""
+    pass
