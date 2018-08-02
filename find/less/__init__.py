dfcd0c11056993ad9f9522afb033ca16f8a23e1f
Chad Sharp <chad@cs50.harvard.edu>
2018-08-02 10:30:48 -0400

    Remove generate from less.

--- find/less/__init__.py
+++ find/less/__init__.py
@@ -9,8 +9,7 @@ def exists():
 @check50.check(exists)
 def compiles():
     """helpers.c compiles."""
-    check50.include("helpers.h", "find.c", "generate.c", "sort.c")
-    check50.c.compile("generate.c", "helpers.c", lcs50=True)
+    check50.include("helpers.h", "find.c", "sort.c")
     check50.c.compile("find.c", "helpers.c", lcs50=True)
     check50.c.compile("sort.c", "helpers.c", lcs50=True)
 
