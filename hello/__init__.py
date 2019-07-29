89de64a416c3909001b40c1e9bca725708a09415
jelleas <jelle.van.assema@gmail.com>
2019-07-29 18:09:55 -0400

    hello world -> hello brian

--- hello/__init__.py
+++ hello/__init__.py
@@ -12,14 +12,11 @@ def compiles():
     check50.c.compile("hello.c", lcs50=True)
 
 @check50.check(compiles)
-def prints_hello():
-    """prints "hello, world\\n" """
-    from re import match
+def veronica():
+    """responds to name Veronica."""
+    check50.run("./hello").stdin("Veronica").stdout("Veronica")
 
-    expected = "[Hh]ello, world!?\n"
-    actual = check50.run("./hello").stdout()
-    if not match(expected, actual):
-        help = None
-        if match(expected[:-1], actual):
-            help = r"did you forget a newline ('\n') at the end of your printf string?"
-        raise check50.Mismatch("hello, world\n", actual, help=help)
+@check50.check(compiles)
+def brian():
+    """responds to name Brian."""
+    check50.run("./hello").stdin("Brian").stdout("Brian")
