e3359d6ebf61a06597d19e730c00a43d29758dd2
Brian Yu <brianyu28@gmail.com>
2019-09-08 14:45:51 -0400

    Add support for music module

--- scratch/__init__.py
+++ scratch/__init__.py
@@ -84,7 +84,7 @@ def uses_variable(project):
 def uses_sound(project):
     """project uses at least one sound"""
 
-    if not contains_blocks(project, ["sound_play", "sound_playuntildone"]):
+    if not contains_blocks(project, ["sound_play", "sound_playuntildone", "music_playNoteForBeat", "music_playDrumForBeats"]):
         raise check50.Failure("no sounds used, 1 required")
 
 
