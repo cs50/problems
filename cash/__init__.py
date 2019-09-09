f8e2b3559988095b0b758147abeabb8397e9f89b
Brian Yu <brianyu28@gmail.com>
2019-09-09 11:51:02 -0400

    Add Problem Set 1 checks

---
+++ cash/__init__.py
@@ -0,0 +1,80 @@
+import check50
+import check50.c
+
+
+@check50.check()
+def exists():
+    """cash exists"""
+    check50.exists("cash.c")
+
+
+@check50.check(exists)
+def compiles():
+    """cash compiles"""
+    check50.c.compile("cash.c", lcs50=True)
+
+
+@check50.check(compiles)
+def test041():
+    """input of 0.41 yields output of 4"""
+    check50.run("./cash").stdin("0.41").stdout(coins(4), "4\n").exit(0)
+
+
+@check50.check(compiles)
+def test001():
+    """input of 0.01 yields output of 1"""
+    check50.run("./cash").stdin("0.01").stdout(coins(1), "1\n").exit(0)
+
+
+@check50.check(compiles)
+def test015():
+    """input of 0.15 yields output of 2"""
+    check50.run("./cash").stdin("0.15").stdout(coins(2), "2\n").exit(0)
+
+
+@check50.check(compiles)
+def test160():
+    """input of 1.6 yields output of 7"""
+    check50.run("./cash").stdin("1.6").stdout(coins(7), "7\n").exit(0)
+
+
+@check50.check(compiles)
+def test230():
+    """input of 23 yields output of 92"""
+    check50.run("./cash").stdin("23").stdout(coins(92), "92\n").exit(0)
+
+
+@check50.check(compiles)
+def test420():
+    """input of 4.2 yields output of 18"""
+    from re import search
+    expected = "18\n"
+    actual = check50.run("./cash").stdin("4.2").stdout()
+    if not search(coins(18), actual):
+        help = None
+        if search(coins(22), actual):
+            help = "did you forget to round your input to the nearest cent?"
+        raise check50.Mismatch(expected, actual, help=help)
+
+
+@check50.check(compiles)
+def test_reject_negative():
+    """rejects a negative input like -1"""
+    check50.run("./cash").stdin("-1").reject()
+
+
+@check50.check(compiles)
+def test_reject_foo():
+    """rejects a non-numeric input of "foo" """
+    check50.run("./cash").stdin("foo").reject()
+
+
+@check50.check(compiles)
+def test_reject_empty():
+    """rejects a non-numeric input of "" """
+    check50.run("./cash").stdin("").reject()
+
+
+def coins(num):
+    # regex that matches `num` not surrounded by any other numbers (so coins(2) won't match e.g. 123)
+    return fr"(?<!\d){num}(?!\d)"
---
+++ credit/__init__.py
@@ -0,0 +1,77 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """credit.c exists."""
+    check50.exists("credit.c")
+
+@check50.check(exists)
+def compiles():
+    """credit.c compiles."""
+    check50.c.compile("credit.c", lcs50=True)
+
+@check50.check(compiles)
+def test1():
+    """identifies 378282246310005 as AMEX"""
+    check50.run("./credit").stdin("378282246310005").stdout("AMEX\n").stdout(check50.EOF).exit(0)
+
+@check50.check(compiles)
+def test2():
+    """identifies 371449635398431 as AMEX"""
+    check50.run("./credit").stdin("371449635398431").stdout("AMEX\n").stdout(check50.EOF).exit(0)
+
+@check50.check(compiles)
+def test3():
+    """identifies 5555555555554444 as MASTERCARD"""
+    check50.run("./credit").stdin("5555555555554444").stdout("MASTERCARD\n").stdout(check50.EOF).exit(0)
+
+@check50.check(compiles)
+def test4():
+    """identifies 5105105105105100 as MASTERCARD"""
+    check50.run("./credit").stdin("5105105105105100").stdout("MASTERCARD\n").stdout(check50.EOF).exit(0)
+
+@check50.check(compiles)
+def test5():
+    """identifies 4111111111111111 as VISA"""
+    check50.run("./credit").stdin("4111111111111111").stdout("VISA\n").stdout(check50.EOF).exit(0)
+
+@check50.check(compiles)
+def test6():
+    """identifies 4012888888881881 as VISA"""
+    check50.run("./credit").stdin("4012888888881881").stdout("VISA\n").stdout(check50.EOF).exit(0)
+
+@check50.check(compiles)
+def test7():
+    """identifies 1234567890 as INVALID"""
+    check50.run("./credit").stdin("1234567890").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+
+@check50.check(compiles)
+def test8():
+    """identifies 369421438430814 as INVALID"""
+    check50.run("./credit").stdin("369421438430814").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+
+@check50.check(compiles)
+def test9():
+    """identifies 4062901840 as INVALID"""
+    check50.run("./credit").stdin("4062901840").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+
+@check50.check(compiles)
+def test10():
+    """identifies 5673598276138003 as INVALID"""
+    check50.run("./credit").stdin("5673598276138003").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+
+@check50.check(compiles)
+def test11():
+    """identifies 4111111111111113 as INVALID"""
+    check50.run("./credit").stdin("4111111111111113").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+
+@check50.check(compiles)
+def test_reject_foo():
+    """rejects a non-numeric input of "foo" """
+    check50.run("./credit").stdin("foo").reject()
+
+@check50.check(compiles)
+def test_reject_empty():
+    """rejects a non-numeric input of "" """
+    check50.run("./credit").stdin("").reject()
