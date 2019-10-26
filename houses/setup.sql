109a48ead32bee181b5ec70dbd7300001ee8797f
Brian Yu <brianyu28@gmail.com>
2019-10-26 11:19:38 -0400

    Add movies, houses checks

---
+++ houses/setup.sql
@@ -0,0 +1,8 @@
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first VARCHAR(255),
+    middle VARCHAR(255),
+    last VARCHAR(255),
+    house VARCHAR(10),
+    birth INTEGER
+);
