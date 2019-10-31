b55c152dbe3f07c79b524acaeb50abe4ee66ec18
Brian Yu <brianyu28@gmail.com>
2019-10-31 13:54:55 -0400

    Add sentimental, DNA checks

---
+++ sentimental/hello/__init__.py
@@ -0,0 +1,16 @@
+import check50
+
+@check50.check()
+def exists():
+    """hello.py exists."""
+    check50.exists("hello.py")
+
+@check50.check(exists)
+def veronica():
+    """responds to name Emma."""
+    check50.run("python3 hello.py").stdin("Emma").stdout("Emma").exit(0)
+
+@check50.check(exists)
+def brian():
+    """responds to name Rodrigo."""
+    check50.run("python3 hello.py").stdin("Rodrigo").stdout("Rodrigo").exit(0)
