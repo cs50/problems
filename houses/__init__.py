e3bbd584e4e46263d7b448454ddeb55972e88151
Brian Yu <brianyu28@gmail.com>
2019-11-07 04:57:49 -0500

    Fix timeout usage

--- houses/__init__.py
+++ houses/__init__.py
@@ -12,7 +12,7 @@ def exists():
 @check50.check(exists)
 def import1():
     """import.py correctly imports Harry Potter"""
-    check50.run("python3 import.py students.csv", timeout=10).exit()
+    check50.run("python3 import.py students.csv").exit(timeout=10)
     db = SQL("sqlite:///students.db")
     rows = db.execute("SELECT first, middle, last, house, birth FROM students WHERE first = 'Harry'")
     expected = [{"first": "Harry", "middle": "James", "last": "Potter", "house": "Gryffindor", "birth": 1980}]
@@ -22,7 +22,7 @@ def import1():
 @check50.check(exists)
 def import2():
     """import.py correctly imports Luna Lovegood"""
-    check50.run("python3 import.py students.csv", timeout=10).exit()
+    check50.run("python3 import.py students.csv").exit(timeout=10)
     db = SQL("sqlite:///students.db")
     rows = db.execute("SELECT first, middle, last, house, birth FROM students WHERE first = 'Luna'")
     expected = [{"first": "Luna", "middle": None, "last": "Lovegood", "house": "Ravenclaw", "birth": 1981}]
@@ -32,7 +32,7 @@ def import2():
 @check50.check(exists)
 def import_count():
     """import.py imports the correct number of rows"""
-    check50.run("python3 import.py students.csv", timeout=10).exit()
+    check50.run("python3 import.py students.csv").exit(timeout=10)
     db = SQL("sqlite:///students.db")
     actual = db.execute("SELECT COUNT(*) as count FROM students")[0]["count"]
     expected = 40
@@ -43,7 +43,7 @@ def import_count():
 def roster_hufflepuff():
     """roster.py produces correct Hufflepuff roster"""
     check50.include("hufflepuff.txt", "hufflepuff_re.txt")
-    actual = check50.run("python3 roster.py Hufflepuff", timeout=10).stdout()
+    actual = check50.run("python3 roster.py Hufflepuff").stdout(timeout=10)
     if not re.search(open("hufflepuff_re.txt").read(), actual):
         raise check50.Mismatch(open("hufflepuff.txt").read(), actual)
 
@@ -51,6 +51,6 @@ def roster_hufflepuff():
 def roster_gryffindor():
     """roster.py produces correct Gryffindor roster"""
     check50.include("gryffindor.txt", "gryffindor_re.txt")
-    actual = check50.run("python3 roster.py Gryffindor", timeout=10).stdout()
+    actual = check50.run("python3 roster.py Gryffindor").stdout(timeout=10)
     if not re.search(open("gryffindor_re.txt").read(), actual):
         raise check50.Mismatch(open("gryffindor.txt").read(), actual)
