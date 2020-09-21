c07c48d235b2f54a2a576556a7c97e2d0a771eb6
Brian Yu <brian@cs.harvard.edu>
2020-09-21 09:37:06 -0400

    Generalize caesar checks

--- caesar/__init__.py
+++ caesar/__init__.py
@@ -14,32 +14,32 @@ def compiles():
 @check50.check(compiles)
 def encrypts_a_as_b():
     """encrypts "a" as "b" using 1 as key"""
-    check50.run("./caesar 1").stdin("a").stdout("ciphertext:\s*b\n", "ciphertext: b\n").exit(0)
+    check50.run("./caesar 1").stdin("a").stdout("[Cc]iphertext:\s*b\n", "ciphertext: b\n").exit(0)
 
 @check50.check(compiles)
 def encrypts_barfoo_as_yxocll():
     """encrypts "barfoo" as "yxocll" using 23 as key"""
-    check50.run("./caesar 23").stdin("barfoo").stdout("ciphertext:\s*yxocll\n", "ciphertext: yxocll\n").exit(0)
+    check50.run("./caesar 23").stdin("barfoo").stdout("[Cc]iphertext:\s*yxocll\n", "ciphertext: yxocll\n").exit(0)
 
 @check50.check(compiles)
 def encrypts_BARFOO_as_EDUIRR():
     """encrypts "BARFOO" as "EDUIRR" using 3 as key"""
-    check50.run("./caesar 3").stdin("BARFOO").stdout("ciphertext:\s*EDUIRR\n", "ciphertext: EDUIRR\n").exit(0)
+    check50.run("./caesar 3").stdin("BARFOO").stdout("[Cc]iphertext:\s*EDUIRR\n", "ciphertext: EDUIRR\n").exit(0)
 
 @check50.check(compiles)
 def encrypts_BaRFoo_FeVJss():
     """encrypts "BaRFoo" as "FeVJss" using 4 as key"""
-    check50.run("./caesar 4").stdin("BaRFoo").stdout("ciphertext:\s*FeVJss\n", "ciphertext: FeVJss\n").exit(0)
+    check50.run("./caesar 4").stdin("BaRFoo").stdout("[Cc]iphertext:\s*FeVJss\n", "ciphertext: FeVJss\n").exit(0)
 
 @check50.check(compiles)
 def encrypts_barfoo_as_onesbb():
     """encrypts "barfoo" as "onesbb" using 65 as key"""
-    check50.run("./caesar 65").stdin("barfoo").stdout("ciphertext:\s*onesbb\n", "ciphertext: onesbb\n").exit(0)
+    check50.run("./caesar 65").stdin("barfoo").stdout("[Cc]iphertext:\s*onesbb\n", "ciphertext: onesbb\n").exit(0)
 
 @check50.check(compiles)
 def checks_for_handling_non_alpha():
     """encrypts "world, say hello!" as "iadxp, emk tqxxa!" using 12 as key"""
-    check50.run("./caesar 12").stdin("world, say hello!").stdout("ciphertext:\s*iadxp, emk tqxxa!\n", "ciphertext: iadxp, emk tqxxa!\n").exit(0)
+    check50.run("./caesar 12").stdin("world, say hello!").stdout("[Cc]iphertext:\s*iadxp, emk tqxxa!\n", "ciphertext: iadxp, emk tqxxa!\n").exit(0)
 
 @check50.check(compiles)
 def handles_no_argv():
