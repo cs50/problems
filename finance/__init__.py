d2945cac5bf1c8fac5a5e45039e250f09f9ce8d1
Brenda Anderson <curiouskiwi@users.noreply.github.com>
2021-03-08 11:26:14 +1300

    Update __init__.py
    removed the /signin hack that is no longer needed.

--- finance/__init__.py
+++ finance/__init__.py
@@ -57,9 +57,6 @@ def register_reject_duplicate_username():
 @check50.check(startup)
 def login_page():
     """login page has all required elements"""
-    if Finance().page_exists("/signin"):
-        Finance().validate_form("/signin", ["username", "password"])
-        return
     Finance().validate_form("/login", ["username", "password"])
 
 
@@ -166,10 +163,7 @@ class Finance(check50.flask.app):
 
     def login(self, username, password):
         """Helper function for logging in"""
-        route = "/login"
-        if self.page_exists("/signin"):
-            route = "/signin"
-        return self.post(route, data={"username": username, "password": password})
+        return self.post("/login", data={"username": username, "password": password})        
 
     def quote(self, ticker):
         """Query app for a quote for `ticker`"""
@@ -209,5 +203,3 @@ class Finance(check50.flask.app):
 
         return self
 
-    def page_exists(self, route):
-        return self.get(route).status() == 200
