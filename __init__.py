b239893fb9d414c1ac58a2a1d80427dcf67e51c0
Brian Yu <brianyu28@gmail.com>
2021-08-05 13:17:28 -0400

    Add experiment

--- __init__.py
+++ __init__.py
@@ -3,16 +3,11 @@ import check50.c
 
 @check50.check()
 def exists():
-    """hello.c exists"""
-    check50.exists("hello.c")
-
-@check50.check(exists)
-def compiles():
-    """hello.c compiles"""
-    check50.c.compile("hello.c", lcs50=True)
+    """experiment.py exists"""
+    check50.exists("experiment.py")
 
 @check50.check(compiles)
 def hello():
     """prints hello, world"""
-    check50.run("./hello").stdout("hello, world").exit()
+    check50.run("python experiment.py").stdout("hello, world").exit()
 
