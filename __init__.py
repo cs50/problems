55cd006173b65ea270d65596975307f9c1236b1b
Brian Yu <brianyu28@gmail.com>
2021-08-05 13:43:58 -0400

    Fix check dependency

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ def exists():
     """experiment.py exists"""
     check50.exists("experiment.py")
 
-@check50.check(compiles)
+@check50.check(exists)
 def hello():
     """prints hello, world"""
     check50.run("python experiment.py").stdout("hello, world").exit()
