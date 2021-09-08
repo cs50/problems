cf017797cd6b12e162b17d31606109a154422d34
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2021-09-08 19:37:31 -0400

    Update function names

--- cash/__init__.py
+++ cash/__init__.py
@@ -47,37 +47,37 @@ def test_reject_foo():
 
 @check50.check(compiles)
 def quarters0():
-    """get_quarters returns 2 when input is 50"""
+    """calculate_quarters returns 2 when input is 50"""
     check50.run("./cash_test 1").stdout("2")
 
 @check50.check(compiles)
 def quarters1():
-    """get_quarters returns 1 when input is 42"""
+    """calculate_quarters returns 1 when input is 42"""
     check50.run("./cash_test 2").stdout("1")
 
 @check50.check(compiles)
 def dimes0():
-    """get_dimes returns 1 when input is 10"""
+    """calculate_dimes returns 1 when input is 10"""
     check50.run("./cash_test 3").stdout("1")
 
 @check50.check(compiles)
 def dimes1():
-    """get_dimes returns 1 when input is 15"""
+    """calculate_dimes returns 1 when input is 15"""
     check50.run("./cash_test 4").stdout("1")
 
 @check50.check(compiles)
 def nickels0():
-    """get_nickels returns 1 when input is 5"""
+    """calculate_nickels returns 1 when input is 5"""
     check50.run("./cash_test 5").stdout("1")
 
 @check50.check(compiles)
 def nickels1():
-    """get_nickels returns 5 when input is 28"""
+    """calculate_nickels returns 5 when input is 28"""
     check50.run("./cash_test 6").stdout("5")
 
 @check50.check(compiles)
 def pennies():
-    """get_pennies returns 4 when input is 4"""
+    """calculate_pennies returns 4 when input is 4"""
     check50.run("./cash_test 7").stdout("4")
 
 @check50.check(compiles)
