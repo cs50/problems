14f3d648fb46279e531315296178d2799839a5b0
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-04-04 19:58:36 -0400

    einstein checks

--- einstein/__init__.py
+++ einstein/__init__.py
@@ -9,7 +9,7 @@ def exists():
 @check50.check(exists)
 def test1():
     """input of 1 yields output of 90000000000000000"""
-    output = check50.run("python3 einstein.py").stdin("1", prompt=False).stdout(r'90,?0{3},?0{3},?0{3}.?(0+)?', "90000000000000000").exit()
+    output = check50.run("python3 einstein.py").stdin("1", prompt=False).stdout(r'(90,?0{3},?0{3},?0{3}.?(0+)?)|(90.?0{3}.?0{3}.?0{3},?(0+)?)', "90000000000000000").exit()
 
 @check50.check(exists)
 def test14():
