3ef382e0311a10cc4763c9b94b6ef88929e04703
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-04-04 19:56:50 -0400

    einstein checks

---
+++ einstein/__init__.py
@@ -0,0 +1,22 @@
+import check50
+import re
+
+@check50.check()
+def exists():
+    """einstein.py exists"""
+    check50.exists("einstein.py")
+
+@check50.check(exists)
+def test1():
+    """input of 1 yields output of 90000000000000000"""
+    output = check50.run("python3 einstein.py").stdin("1", prompt=False).stdout(r'90,?0{3},?0{3},?0{3}.?(0+)?', "90000000000000000").exit()
+
+@check50.check(exists)
+def test14():
+    """input of 14 yields output of 1260000000000000000"""
+    check50.run("python3 einstein.py").stdin("14", prompt=False).stdout(r'(1,?260,?0{3},?0{3},?0{3},?0{3},?0{3}.?(0+)?)|(1.?260.?0{3}.?0{3}.?0{3}.?0{3}.?0{3},?(0+)?)',"1260000000000000000").exit()
+
+@check50.check(exists)
+def test50():
+    """input of 50 yields output of 4500000000000000000"""
+    check50.run("python3 einstein.py").stdin("50", prompt=False).stdout(r'(4,?500,?0{3},?0{3},?0{3},?0{3},?0{3}.?(0+)?)|(4.?500.?0{3}.?0{3}.?0{3}.?0{3}.?0{3},?(0+)?)', "4500000000000000000").exit()
