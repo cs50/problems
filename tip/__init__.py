45db48cb5d4fd0ce7f892671b31a3147495ec195
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-04-04 20:26:54 -0400

    tip checks

---
+++ tip/__init__.py
@@ -0,0 +1,22 @@
+import check50
+
+
+@check50.check()
+def exists():
+    """tip.py exists"""
+    check50.exists("tip.py")
+
+@check50.check(exists)
+def test50():
+    """input of $50.00 and 15% yields $7.50"""
+    check50.run("python3 tip.py").stdin("$50.00", prompt=True).stdin("15%", prompt=True).stdout(r'\$?7.50', '7.50').exit()
+
+@check50.check(exists)
+def test100():
+    """input of $100.00 and 18% yields $18.00"""
+    check50.run("python3 tip.py").stdin("$100.00", prompt=True).stdin("18%", prompt=True).stdout(r'\$?18.00', '18.00').exit()
+
+@check50.check(exists)
+def test15():
+    """input of $15.00 and 25% yields $3.75"""
+    check50.run("python3 tip.py").stdin("$15.00", prompt=True).stdin("25%", prompt=True).stdout(r'\$?3.75', '3.75').exit()
