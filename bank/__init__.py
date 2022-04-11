2a78f185f7ed880e89923b69cfd04ade8045711e
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-04-11 17:22:53 -0400

    clean bank checks

--- bank/__init__.py
+++ bank/__init__.py
@@ -1,4 +1,5 @@
 import check50
+from re import escape
 
 
 @check50.check()
@@ -10,34 +11,51 @@ def exists():
 @check50.check(exists)
 def testHello():
     """input of \"Hello\" yields output of $0"""
-    check50.run("python3 bank.py").stdin("Hello", prompt=True).stdout(r'^[$]0[^\d]$', "$0", regex=True).exit()
+    input = "Hello"
+    output = "$0"
+    check50.run("python3 bank.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 @check50.check(exists)
 def testHello_spaces():
     """input of \" Hello \" yields output of $0"""
-    check50.run("python3 bank.py").stdin(" Hello ", prompt=True).stdout(r'^[$]0[^\d]$', "$0", regex=True).exit()
+    input = " Hello "
+    output = "$0"
+    check50.run("python3 bank.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 @check50.check(exists)
 def testHello_Newman():
     """input of \"Hello, Newman\" yields output of $0"""
-    check50.run("python3 bank.py").stdin("Hello, Newman", prompt=True).stdout(r'^[$]0[^\d]$', "$0", regex=True).exit()
+    input = "Hello, Newman"
+    output = "$0"
+    check50.run("python3 bank.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 @check50.check(exists)
 def testHow_you_doing():
     """input of \"How you doing?\" yields output of $20"""
-    check50.run("python3 bank.py").stdin("How you doing?", prompt=True).stdout(r'^[$]20[^\d]$', "$20", regex=True).exit()
+    input = "How you doing?"
+    output = "$20"
+    check50.run("python3 bank.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 @check50.check(exists)
 def testWhats_happening():
     """input of \"What's happening?\" yields output of $100"""
-    check50.run("python3 bank.py").stdin("What's happening?", prompt=True).stdout(r'^[$]100[^\d]$', "$100", regex=True).exit()
+    input = "What's happening?"
+    output = "$100"
+    check50.run("python3 bank.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 @check50.check(exists)
 def testWhats_up():
     """input of \"what's up?\" yields output of $100"""
-    check50.run("python3 bank.py").stdin("What's up?", prompt=True).stdout(r'^[$]100[^\d]$', "$100", regex=True).exit()
+    input = "What's up?"
+    output = "$100"
+    check50.run("python3 bank.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+def regex(amount):
+    """match amount, allowing for characters (not numbers) on either side"""
+    return fr'^[^\d]*{escape(amount)}[^\d]*$'
