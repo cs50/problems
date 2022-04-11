1d1e069af2988bc70cf0233a66fa33b934c3119f
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-04-11 16:52:07 -0400

    meal checks

---
+++ meal/__init__.py
@@ -0,0 +1,53 @@
+import check50
+from re import escape
+
+
+@check50.check()
+def exists():
+    """meal.py exists"""
+    check50.exists("meal.py")
+
+
+@check50.check(exists)
+def test_breakfast():
+    """input of 7:00 yields output of \"breakfast time\""""
+    input = "7:00"
+    output = "breakfast time"
+    check50.run("python3 meal.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_breakfast2():
+    """input of 7:30 yields output of \"breakfast time\""""
+    input = "7:30"
+    output = "breakfast time"
+    check50.run("python3 meal.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_lunch():
+    """input of 12:42 yields output of \"lunch time\""""
+    input = "12:42"
+    output = "lunch time"
+    check50.run("python3 meal.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_dinner():
+    """input of 18:32 yields output of \"dinner time\""""
+    input = "18:32"
+    output = "dinner time"
+    check50.run("python3 meal.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_no_output():
+    """input of 14:30 yields no output"""
+    input = "14:30"
+    output = ""
+    check50.run("python3 meal.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+def regex(time):
+    """match input strictly, with only whitespace on either side"""
+    return fr'^\s*{time}\s*$'
