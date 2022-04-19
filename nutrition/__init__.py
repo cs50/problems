bcf61e74686964d76d5c01a71dd1369506e57815
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-04-18 22:08:11 -0400

    nutrition checks

---
+++ nutrition/__init__.py
@@ -0,0 +1,53 @@
+import check50
+from re import escape
+
+
+@check50.check()
+def exists():
+    """nutrition.py exists"""
+    check50.exists("nutrition.py")
+
+
+@check50.check(exists)
+def test_apple():
+    """input of apple yields output of 130"""
+    input = "apple"
+    output = "130"
+    check50.run("python3 nutrition.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_avocado():
+    """input of Avocado yields output of 50"""
+    input = "Avocado"
+    output = "50"
+    check50.run("python3 nutrition.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_kiwifruit():
+    """input of Kiwifruit yields output of 90"""
+    input = "Kiwifruit"
+    output = "90"
+    check50.run("python3 nutrition.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_pear():
+    """input of pear yields output of 100"""
+    input = "pear"
+    output = "100"
+    check50.run("python3 nutrition.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_sweet_cherries():
+    """input of Sweet Cherries yields output of 100"""
+    input = "Sweet Cherries"
+    output = "100"
+    check50.run("python3 nutrition.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+def regex(text):
+    """match case-sensitively, allowing for characters (but not numbers) on either side"""
+    return fr'^[^\d]*{escape(text)}[^\d]*$'
