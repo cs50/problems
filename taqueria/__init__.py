5f7238d44b0f45ed56e3ccd25e8aa01aa809fbae
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-04-25 15:25:17 -0400

    taqueria checks

--- taqueria/__init__.py
+++ taqueria/__init__.py
@@ -1,18 +1,6 @@
 import check50
 from re import escape
 
-MENU = {
-    "Baja Taco": 4.00,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
-    "Quesadilla": 8.50,
-    "Super Burrito": 8.50,
-    "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00
-}
-
 
 @check50.check()
 def exists():
