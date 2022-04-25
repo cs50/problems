2f5656a6f1106bb497ef745707d46ce4e26f71ee
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-04-25 15:24:37 -0400

    taqueria checks

---
+++ taqueria/__init__.py
@@ -0,0 +1,64 @@
+import check50
+from re import escape
+
+MENU = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+
+@check50.check()
+def exists():
+    """taqueria.py exists"""
+    check50.exists("taqueria.py")
+
+
+@check50.check(exists)
+def test_basic_order():
+    """input of \"taco\", \"taco\", and \"tortilla salad\" results in $14.00"""
+    items = ["taco", "taco", "tortilla salad"]
+    output = 14.0
+    check50.run("python3 taqueria.py").stdin(items[0], prompt=True).stdin(items[1], prompt=True).stdin(items[2], prompt=True).stdout(regex(f"{output:.2f}"), f"${output:.2f}", regex=True).kill()
+
+
+@check50.check(exists)
+def test_basic_order_2():
+    """input of \"burrito\", \"bowl\", and \"nachos\" results in $27.00"""
+    items = ["burrito", "bowl", "nachos"]
+    output = 27.0
+    check50.run("python3 taqueria.py").stdin(items[0], prompt=True).stdin(items[1], prompt=True).stdin(items[2], prompt=True).stdout(regex(f"{output:.2f}"), f"${output:.2f}", regex=True).kill()
+
+
+@check50.check(exists)
+def test_basic_order_3():
+    """input of \"Baja Taco\", \"Quesadilla\", and \"Super Burrito\" results in $21.00"""
+    items = ["Baja Taco", "Quesadilla", "Super Burrito"]
+    output = 21.0
+    check50.run("python3 taqueria.py").stdin(items[0], prompt=True).stdin(items[1], prompt=True).stdin(items[2], prompt=True).stdout(regex(f"{output:.2f}"), f"${output:.2f}", regex=True).kill()
+
+
+@check50.check(exists)
+def test_capitalization():
+    """input of \"Super quesadilla\" results in $9.50"""
+    input = "Super quesadilla"
+    output = 9.50
+    check50.run("python3 taqueria.py").stdin(input, prompt=True).stdout(regex(f"{output:.2f}"), f"${output:.2f}", regex=True).kill()
+
+
+@check50.check(exists)
+def test_invalid_order():
+    """input of \"burger\" results in reprompt"""
+    input = "burger"
+    check50.run("python3 taqueria.py").stdin(input, prompt=True).reject()
+
+
+def regex(cost):
+    """match case-insensitively with dollar-sign required and only characters on either side"""
+    return fr'(?i)^[\D]*\${escape(cost)}[\D]*$'
