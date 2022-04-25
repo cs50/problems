b0bac5a6bb9656a615358d458498a03aa4f7777a
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-04-25 14:26:59 -0400

    fuel checks

---
+++ fuel/__init__.py
@@ -0,0 +1,83 @@
+import check50
+from re import escape
+
+
+@check50.check()
+def exists():
+    """fuel.py exists"""
+    check50.exists("fuel.py")
+
+
+@check50.check(exists)
+def test_3_over_4():
+    """input of 3/4 yields output of 75%"""
+    input = "3/4"
+    output = "75%"
+    check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_round():
+    """input of 503/1000 yields output of 50%"""
+    input = "503/1000"
+    output = "50%"
+    check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_empty():
+    """input of 0/100 yields output of E"""
+    input = "0/100"
+    output = "E"
+    check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_almost_empty():
+    """input of 1/1000 yields output of E"""
+    input = "1/1000"
+    output = "E"
+    check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_almost_empty():
+    """input of 8/1000 yields output of E"""
+    input = "8/1000"
+    output = "E"
+    check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_full():
+    """input of 100/100 yields output of F"""
+    input = "100/100"
+    output = "F"
+    check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_almost_full():
+    """input of 992/1000 yields output of F"""
+    input = "992/1000"
+    output = "F"
+    check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_ZeroDivisionError():
+    """input of 100/0 results in reprompt"""
+    input = "100/0"
+    check50.run("python3 fuel.py").stdin(input, prompt=True).reject()
+
+
+@check50.check(exists)
+def test_ValueError():
+    """input of three/four results in reprompt"""
+    input = "three/four"
+    check50.run("python3 fuel.py").stdin(input, prompt=True).reject()
+
+
+def regex(percent):
+    """match case-insensitively with only whitespace on either side"""
+    return fr'(?i)^\s*{escape(percent)}\s*$'
