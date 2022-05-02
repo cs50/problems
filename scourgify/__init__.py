e35bc4e1761a63c99d7f2033941856db14e6abc4
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-05-02 18:39:46 -0400

    scourgify checks

--- scourgify/__init__.py
+++ scourgify/__init__.py
@@ -1,4 +1,5 @@
 import check50
+import check50.internal
 from re import escape
 
 
@@ -9,28 +10,43 @@ def exists():
     check50.include("before.csv")
 
 
-@check50.check()
+@check50.check(exists)
 def test_no_arguments():
     """scourgify.py exits given no command-line arguments"""
     check50.run("python3 scourgify.py").exit(1)
 
 
-@check50.check()
+@check50.check(exists)
 def test_too_few_arguments():
     """scourgify.py exits given too few command-line arguments"""
     check50.run("python3 scourgify.py 1.csv").exit(1)
 
 
-@check50.check()
+@check50.check(exists)
 def test_too_many_arguments():
     """scourgify.py exits given too many command-line arguments"""
     check50.run("python3 scourgify.py 1.csv 2.csv 3.csv").exit(1)
 
 
-@check50.check()
+@check50.check(exists)
 def test_invalid_file():
     """scourgify.py exits given invalid file"""
     check50.run("python3 scourgify.py 1.csv 2.csv").exit(1)
+
+
+@check50.check(exists)
+def test_create_file():
+    """scourgify.py creates new csv file"""
+    check50.run("python3 scourgify.py before.csv after.csv").exit(0)
+    check50.exists("after.csv")
+
+
+@check50.check(test_create_file)
+def test_clean_file():
+    """scourgify.py cleans csv file"""
+    check50.run("python3 scourgify.py before.csv after.csv").exit(0)
+    if check50.hash("after.csv") != "440790c127f56d3581809a6e5feef891a49f0039aff7944035afdf4a75aec170":
+        raise check50.Failure("csv does not match specified format")
 
 
 def regex(text):
