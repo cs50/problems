e14ee6de9f460470e66e81638569c74a44909cb1
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-05-02 18:15:17 -0400

    draft scourgify checks

--- scourgify/__init__.py
+++ scourgify/__init__.py
@@ -1,61 +1,31 @@
 import check50
-from pexpect import EOF
 from re import escape
-import requests
-import sys
 
 
 @check50.check()
 def exists():
-    """bitcoin.py exists"""
-    check50.exists("bitcoin.py")
+    """scourgify.py exists"""
+    check50.exists("scourgify.py")
 
 
 @check50.check()
 def test_no_arguments():
-    """bitcoin.py exits given no command-line argument"""
-    check50.run("python3 bitcoin.py").exit(1)
+    """scourgify.py exits given no command-line arguments"""
+    check50.run("python3 scourgify.py").exit(1)
 
 
 @check50.check()
-def test_non_numeric_argument():
-    """bitcoin.py exits given non-numeric command-line argument"""
-    check50.run("python3 bitcoin.py cat").exit(1)
+def test_too_few_arguments():
+    """scourgify.py exits given too few command-line arguments"""
+    check50.run("python3 scourgify.py 1.csv").exit(1)
 
 
 @check50.check()
-def test_single_coin():
-    """bitcoin.py provides price of 1 Bitcoin to 4 decimal places"""
-    coins = 1
-    price = get_price(coins)
-    check50.run(f"python3 bitcoin.py {coins}").stdout(regex(price), price, regex=True).exit(0)
-
-
-@check50.check()
-def test_two_coins():
-    """bitcoin.py provides price of 2 Bitcoin to 4 decimal places"""
-    coins = 2
-    price = get_price(coins)
-    check50.run(f"python3 bitcoin.py {coins}").stdout(regex(price), price, regex=True).exit(0)
-
-
-@check50.check()
-def test_decimal_coins():
-    """bitcoin.py provides price of 2.5 Bitcoin to 4 decimal places"""
-    coins = 2.5
-    price = get_price(coins)
-    check50.run(f"python3 bitcoin.py {coins}").stdout(regex(price), price, regex=True).exit(0)
+def test_too_many_arguments():
+    """scourgify.py exits given too many command-line arguments"""
+    check50.run("python3 scourgify.py 1.csv 2.csv 3.csv").exit(1)
 
 
 def regex(text):
     """match case-sensitively with any characters preceding and only whitespace after"""
     return fr'^.*{escape(text)}\s*$'
-
-
-def get_price(coins):
-    try:
-        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-        price = response.json()["bpi"]["USD"]["rate_float"]
-    except requests.HTTPError:
-        sys.exit("check50 encountered a server error")
-    return f"${coins * price:,.4f}"
