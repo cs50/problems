4d099d2edeea86f55620bb3835dcae2d739f5895
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-05-03 11:31:12 -0400

    remove testing.py approach

--- game/__init__.py
+++ game/__init__.py
@@ -11,52 +11,52 @@ def exists():
 @check50.check(exists)
 def test_string_level():
     """game.py rejects non-numeric level"""
-    check50.run("python3 testing.py").stdin("cat", prompt=True).reject()
+    check50.run("python3 game.py").stdin("cat", prompt=True).reject()
 
 
 @check50.check(exists)
 def test_integer_level():
     """game.py rejects out-of-range level"""
-    check50.run("python3 testing.py").stdin("0", prompt=True).reject()
+    check50.run("python3 game.py").stdin("0", prompt=True).reject()
 
 
 @check50.check(exists)
 def test_valid_level():
     """game.py accepts valid level"""
-    check50.run("python3 testing.py").stdin("10", prompt=True).stdout(regex("Guess"), "Guess:", regex=True).kill()
+    check50.run("python3 game.py").stdin("10", prompt=True).stdout(regex("Guess"), "Guess:", regex=True).kill()
 
 
 @check50.check(test_valid_level)
 def test_string_guess():
     """game.py rejects non-numeric guess"""
-    check50.run("python3 testing.py").stdin("1", prompt=True).stdin("cat", prompt=True).reject()
+    check50.run("python3 game.py").stdin("1", prompt=True).stdin("cat", prompt=True).reject()
 
 
 @check50.check(test_valid_level)
 def test_integer_guess():
     """game.py rejects out-of-range guess"""
-    check50.run("python3 testing.py").stdin("1", prompt=True).stdin("0", prompt=True).reject()
+    check50.run("python3 game.py").stdin("1", prompt=True).stdin("0", prompt=True).reject()
 
 
 @check50.check(test_valid_level)
 def test_too_large():
     """game.py outputs \"Too large!\" when guess is too large"""
     output = "Too large!"
-    check50.run("python3 testing.py").stdin("5", prompt=True).stdin("10", prompt=True).stdout(regex(output), output, regex=True).reject()
+    check50.run("python3 game.py").stdin("5", prompt=True).stdin("10", prompt=True).stdout(regex(output), output, regex=True).reject()
 
 
 @check50.check(test_valid_level)
 def test_just_right():
     """game.py outputs \"Just right!\" when guess is correct"""
     output = "Just right!"
-    check50.run("python3 testing.py").stdin("1", prompt=True).stdin("1", prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 game.py").stdin("1", prompt=True).stdin("1", prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 @check50.check(test_valid_level)
 def test_too_small():
     """game.py outputs \"Too small!\" when guess is too low"""
     output = "Too small!"
-    check50.run("python3 testing.py").stdin("1000000000", prompt=True).stdin("1", prompt=True).stdout(regex(output), output, regex=True).reject()
+    check50.run("python3 game.py").stdin("1000000000", prompt=True).stdin("1", prompt=True).stdout(regex(output), output, regex=True).reject()
 
 
 def regex(text):
