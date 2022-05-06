43e8ee93a29e36e4b5c4df14e69ce9f4eded9f9f
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-05-05 20:53:24 -0400

    >1 args to randrange

--- game/testing.py
+++ game/testing.py
@@ -2,7 +2,7 @@ import random
 
 # Monkey-patch randint and randrange
 random.randint = lambda x, y : 4
-random.randrange = lambda x, y : 4
+random.randrange = lambda x, *args, **kwargs : 4
 
 # Run game via import
 import game
