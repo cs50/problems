17e953d154d47880039f25a21af2bbd7c26fd246
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-05-10 15:41:24 -0400

    obfuscate plates checks

--- tests/plates/compile.py
+++
@@ -1,6 +0,0 @@
-from py_compile import compile
-
-FILES = ["alnum_test.py", "beginning_alpha_test.py", "correct_test.py", "length_test.py", "number_test.py", "zero_test.py"]
-
-for file in FILES:
-    compile(file, f"{file[:-3]}.pyc")
