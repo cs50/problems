8c03c2106524ebd8308c5695390bf40387b3fbbf
Rongxin Liu <rongxinliu.dev@gmail.com>
2022-05-10 16:11:42 -0400

    test

---
+++ tests/plates/compileall.py
@@ -0,0 +1,8 @@
+import py_compile
+
+py_compile.compile('alnum_test.py', 'alnum_test.pyc')
+py_compile.compile('beginning_alpha_test.py', 'beginning_alpha_test.pyc')
+py_compile.compile('correct_test.py', 'correct_test.pyc')
+py_compile.compile('length_test.py', 'length_test.pyc')
+py_compile.compile('number_test.py', 'number_test.pyc')
+py_compile.compile('zero_test.py', 'zero_test.pyc')
