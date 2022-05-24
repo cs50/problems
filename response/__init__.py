9d81169deaaed7958630536f4c8799467a447872
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-05-24 15:24:24 -0400

    update libraries check

--- response/__init__.py
+++ response/__init__.py
@@ -10,16 +10,16 @@ def exists():
 
 @check50.check(exists)
 def libraries():
-    """response.py uses either the validators or validator-collection library and does not use the re library"""
+    """response.py does not use re and uses either validators or validator-collection"""
     with open("response.py", "r") as file:
         contents = file.read()
-        if not search(r'(?<!#[ \t\w]*)((import[ \t]*(validators|validator_collection))|(from[ \t]*(validators|validator_collection)[ \t]*import[ \t]*[\w\*]+))', contents):
+        if not search(r'(?<!#)(?<! )((import[ \t]*(validators|validator_collection))|(from[ \t]*(validators|validator_collection)[ \t]*import[ \t]*[\w\*]+))', contents):
             raise check50.Failure("response.py does not import validators or validator-collection library", help="Did you forget to include \"import validators\" or \"import validator_collection\"?")
-        if search(r'(?<!#[ \t]*)((import[ \t]*re)|(from[ \t]*re[ \t]*import[ \t]*[\w\*]+))', contents):
-            raise check50.Failure("response.py uses the re library", help="Be sure not to import functions from the re library, either via \"import re\" or \"from re import ...\"")
+        if search(r'(?<!#)(?<! )((import[ \t]*re)|(from[ \t]*re[ \t]*import[ \t]*[\w\*]+))', contents):
+            raise check50.Failure("response.py uses the re library", help="Be sure not to import functions from the re library, either via \"import re\" or via \"from re import ...\"")
 
 
-@check50.check(exists)
+@check50.check(libraries)
 def test_valid_email():
     """response.py yields Valid when email address is \"malan@harvard.edu\""""
     input = "malan@harvard.edu"
@@ -27,7 +27,7 @@ def test_valid_email():
     check50.run("python3 response.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit(0)
 
 
-@check50.check(exists)
+@check50.check(libraries)
 def test_valid_email_2():
     """response.py yields Valid when email address is \"sysadmins@cs50.harvard.edu\""""
     input = "sysadmins@cs50.harvard.edu"
@@ -35,7 +35,7 @@ def test_valid_email_2():
     check50.run("python3 response.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit(0)
 
 
-@check50.check(exists)
+@check50.check(libraries)
 def test_invalid_email_spelled_out():
     """response.py yields Invalid when email address is \"malan at harvard dot edu\""""
     input = "malan at harvard dot edu"
@@ -43,7 +43,7 @@ def test_invalid_email_spelled_out():
     check50.run("python3 response.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit(0)
 
 
-@check50.check(exists)
+@check50.check(libraries)
 def test_invalid_email_at_symbols():
     """response.py yields Invalid when email address is \"malan@@@harvard.edu\""""
     input = "malan@@@harvard.edu"
