ace5a9972760243f051533240bc73caaf56de9f9
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-06-02 21:32:17 -0400

    fix testing file

--- seasons/testing.py
+++ seasons/testing.py
@@ -1,15 +1,27 @@
 from datetime import date
 import seasons
 
-
+# Create subclass of date class with patched method, today
 class mockDate(date):
     def today():
         return date(2000, 1, 1)
 
 
-try:
-    seasons.datetime.date = mockDate
-except AttributeError:
-    seasons.date = mockDate
+# Override student's import of date class with testing class
+seasons.date = mockDate
 
+try:
+
+    # If student has imported datetime module as a whole, override date class inside of datetime module
+    seasons.datetime.date = mockDate
+except TypeError:
+
+    # Student has instead imported datetime class from datetime module: nothing to do
+    pass
+except AttributeError:
+
+    # Student has not imported datetime module at all: nothing to do
+    pass
+
+# Run student program
 seasons.main()
