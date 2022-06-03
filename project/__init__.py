e0e24bae1d0576b7dcc513fbbd928be499cd200a
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-06-03 14:42:38 -0400

    style

--- project/__init__.py
+++ project/__init__.py
@@ -1,10 +1,12 @@
 import re
 import check50
 
+
 @check50.check()
 def exists_readme():
     """README.md exists"""
     check50.exists("README.md")
+
 
 @check50.check(exists_readme)
 def final_readme():
@@ -12,26 +14,30 @@ def final_readme():
     text = open("README.md").read().lower()
     if len(text) < 2500:
         raise check50.Failure(f"Description is not long enough.")
-       
-    urls = re.findall('https?:\/\/[\w/\-?=%.]+\.[\w/\-?=%.]+', text)
+
+    urls = re.findall("https?:\/\/[\w/\-?=%.]+\.[\w/\-?=%.]+", text)
     if not urls:
         raise check50.Failure(f"Video URL is missing.")
+
 
 @check50.check()
 def exists_project():
     """project.py exists"""
     check50.exists("project.py")
 
+
 @check50.check(exists_project)
 def main_function():
     """main function exists"""
     check50.run("echo 'from project import main' | python3").exit(0)
+
 
 @check50.check(exists_project)
 def custom_functions():
     """implemented at least 3 top-level functions other than main"""
     check50.include("custom_functions_check.py")
     check50.run("python3 custom_functions_check.py custom_functions").exit(0)
+
 
 @check50.check(custom_functions)
 def unit_test():
