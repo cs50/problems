934ead497ee3961016db8677e22e45549400d0aa
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2023-07-21 17:07:48 -0400

    stage `2023/sql` branch

--- houses/setup.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE students (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    first VARCHAR(255),
-    middle VARCHAR(255),
-    last VARCHAR(255),
-    house VARCHAR(10),
-    birth INTEGER
-);
