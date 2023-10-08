7cda8f9670e1c23a15f29485b57bb0532505d8b6
Brenda Anderson <curiouskiwi@users.noreply.github.com>
2023-10-08 13:38:28 +1300

    Update __init__.py
    changed check to verify that hack.py actually changes the password.

--- sentimental/dont-panic/python/__init__.py
+++ sentimental/dont-panic/python/__init__.py
@@ -10,5 +10,11 @@ def exists():
 
 @check50.check(exists)
 def test_execution():
-    """hack.py runs without error"""
-    check50.run("python hack.py").exit(0)
+    """hack.py correctly changes admin password"""
+    new_password = "CS50"
+    check50.run("python3 hack.py").stdin(new_password).exit(timeout=10)
+    db = SQL("sqlite:///dont-panic.db")
+    result = db.execute("SELECT password FROM users WHERE username = 'admin'")[0]["password"]
+    if result != new_password:
+        raise check50.Mismatch(str(expected), str(rows))    
+
