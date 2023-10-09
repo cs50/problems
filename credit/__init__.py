39b190c0fc68c1db3ab88a29ceda67346c8d989c
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2023-10-08 20:01:23 -0400

    add credit updates to 2023/x

--- credit/__init__.py
+++ credit/__init__.py
@@ -48,30 +48,40 @@ def test7():
 
 @check50.check(compiles)
 def test8():
-    """identifies 1234567890 as INVALID"""
+    """identifies 1234567890 as INVALID (invalid length, checksum, identifying digits)"""
     check50.run("./credit").stdin("1234567890").stdout("INVALID\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test9():
-    """identifies 369421438430814 as INVALID"""
+    """identifies 369421438430814 as INVALID (invalid identifying digits)"""
     check50.run("./credit").stdin("369421438430814").stdout("INVALID\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test10():
-    """identifies 4062901840 as INVALID"""
+    """identifies 4062901840 as INVALID (invalid length)"""
     check50.run("./credit").stdin("4062901840").stdout("INVALID\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test11():
-    """identifies 5673598276138003 as INVALID"""
+    """identifies 5673598276138003 as INVALID (invalid identifying digits)"""
     check50.run("./credit").stdin("5673598276138003").stdout("INVALID\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test12():
-    """identifies 4111111111111113 as INVALID"""
+    """identifies 4111111111111113 as INVALID (invalid checksum)"""
     check50.run("./credit").stdin("4111111111111113").stdout("INVALID\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test13():
-    """identifies 4222222222223 as INVALID"""
+    """identifies 4222222222223 as INVALID (invalid checksum)"""
     check50.run("./credit").stdin("4222222222223").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+
+@check50.check(compiles)
+def test14():
+    """identifies 3400000000000620 as INVALID (AMEX identifying digits, VISA/Mastercard length)"""
+    check50.run("./credit").stdin("3400000000000620").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+
+@check50.check(compiles)
+def test15():
+    """identifies 430000000000000 as INVALID (VISA identifying digits, AMEX length)"""
+    check50.run("./credit").stdin("430000000000000").stdout("INVALID\n").stdout(check50.EOF).exit(0)
