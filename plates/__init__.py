10541614d5bc6d3f47c8ec0c27e06ded6a972a5a
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2023-11-27 09:04:10 -0600

    update style

--- plates/__init__.py
+++ plates/__init__.py
@@ -13,7 +13,9 @@ def test_cs50():
     """input of CS50 yields output of Valid"""
     input = "CS50"
     output = "Valid"
-    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(
+        regex(output), output, regex=True
+    ).exit()
 
 
 # Ready Player One
@@ -22,7 +24,9 @@ def test_ECTO88():
     """input of ECTO88 yields output of Valid"""
     input = "ECTO88"
     output = "Valid"
-    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(
+        regex(output), output, regex=True
+    ).exit()
 
 
 # Ferris Buehler's Day Off
@@ -31,7 +35,9 @@ def test_NRVOUS():
     """input of NRVOUS yields output of Valid"""
     input = "NRVOUS"
     output = "Valid"
-    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(
+        regex(output), output, regex=True
+    ).exit()
 
 
 # Tests for Zero as first number
@@ -40,7 +46,9 @@ def test_CS05():
     """input of CS05 yields output of Invalid"""
     input = "CS05"
     output = "Invalid"
-    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(
+        regex(output), output, regex=True
+    ).exit()
 
 
 # First two chars are not letters
@@ -48,7 +56,9 @@ def test_50():
     """input of 50 yields output of Invalid"""
     input = "50"
     output = "Invalid"
-    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(
+        regex(output), output, regex=True
+    ).exit()
 
 
 # Numbers before letters (after the first 2 letters)
@@ -57,7 +67,9 @@ def test_CS50P2():
     """input of CS50P2 yields output of Invalid"""
     input = "CS50P2"
     output = "Invalid"
-    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(
+        regex(output), output, regex=True
+    ).exit()
 
 
 # Non-alphanumeric characters (after the first 2 letters)
@@ -66,7 +78,9 @@ def test_PI3_14():
     """input of PI3.14 yields output of Invalid"""
     input = "PI3.14"
     output = "Invalid"
-    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(
+        regex(output), output, regex=True
+    ).exit()
 
 
 # Length (too short)
@@ -75,7 +89,9 @@ def test_445108():
     """input of H yields output of Invalid"""
     input = "H"
     output = "Invalid"
-    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(
+        regex(output), output, regex=True
+    ).exit()
 
 
 # Length (too long) (Back to the Future)
@@ -84,9 +100,11 @@ def test_OUTATIME():
     """input of OUTATIME yields output of Invalid"""
     input = "OUTATIME"
     output = "Invalid"
-    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 plates.py").stdin(input, prompt=True).stdout(
+        regex(output), output, regex=True
+    ).exit()
 
 
 def regex(text):
     """match case-insensitively, allowing for only spaces on either side"""
-    return fr'(?i)^\s*{escape(text)}\s*$'
+    return rf"(?i)^\s*{escape(text)}\s*$"
