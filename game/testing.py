cd407899999d2f5d003513f754f6ca0866d90846
Makaze <christopherslane.work@gmail.com>
2023-12-18 08:57:54 -0500

    Make game call main only if it contains a __name__ check

--- game/testing.py
+++ game/testing.py
@@ -1,17 +1,23 @@
 import random
+import re
+import inspect
 
 # Monkey-patch randint, randrange, choice
-random.randint = lambda x, y : 4
-random.randrange = lambda x, *args, **kwargs : 4
-random.choice = lambda x : 4
+random.randint = lambda x, y: 4
+random.randrange = lambda x, *args, **kwargs: 4
+random.choice = lambda x: 4
 
 # Run game via import
 import game
 
 # Run game if not run when imported
 try:
-    game.main()
-except AttributeError:
+    # Get commentless source
+    source = re.sub(r"#.*(\n|$)", "", inspect.getsource(game))
+    # Look for __name__ check
+    if '__name__ == "__main__":' in source:
+        game.main()
 
+except AttributeError:
     # game has no main function
     pass
