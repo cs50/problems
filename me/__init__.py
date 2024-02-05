2dc4ea21324e7f3a5f5e646b724c63662b2a6433
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2024-02-05 11:49:32 -0500

    checkout changes from 2024/spring

--- me/__init__.py
+++ me/__init__.py
@@ -1,27 +1,53 @@
 import check50
 import check50.c
+import re
+
 
 @check50.check()
 def exists():
     """hello.c exists"""
     check50.exists("hello.c")
 
+
 @check50.check(exists)
 def compiles():
     """hello.c compiles"""
     check50.c.compile("hello.c", lcs50=True)
 
-@check50.check(compiles)
-def emma():
-    """responds to name Emma"""
-    check50.run("./hello").stdin("Emma").stdout("Emma").exit()
 
 @check50.check(compiles)
-def inno():
-    """responds to name Inno"""
-    check50.run("./hello").stdin("Inno").stdout("Inno").exit()
+def mario():
+    """responds to name Mario"""
+    check_name("Mario")
+
 
 @check50.check(compiles)
-def kamryn():
-    """responds to name Kamryn"""
-    check50.run("./hello").stdin("Kamryn").stdout("Kamryn").exit()
+def peach():
+    """responds to name Peach"""
+    check_name("Peach")
+
+
+@check50.check(compiles)
+def bowser():
+    """responds to name Bowser"""
+    check_name("Bowser")
+
+
+def check_name(name):
+    # Define expected, actual outputs
+    expected = f"hello, {name}\n"
+    actual = check50.run("./hello").stdin(name).stdout()
+
+    # Check output
+    if not re.match(regex(name), actual):
+        if actual[-1] != "\n":
+            raise check50.Mismatch(
+                expected=expected,
+                actual=actual,
+                help=r"Forgot to print a newline at the end of your output?",
+            )
+        raise check50.Mismatch(expected=expected, actual=actual)
+
+
+def regex(string):
+    return f"^[Hh]ello, {re.escape(string)}\n$"
