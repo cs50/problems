3ea473009d1940b985f5c46de2d2f71ca9aa342b
juanxcalderon <67012503+juanxcalderon@users.noreply.github.com>
2025-05-17 19:08:11 -0400

    Update __init__.py

--- mario/complejo/__init__.py
+++ mario/complejo/__init__.py
@@ -1,4 +1,4 @@
-less = __import__("check50").import_checks("../less")
-from less import *
+less = __import__("check50").import_checks("../sencillo")
+from sencillo import *
 
 # mario/more checks are identical to the mario/less checks just with different txt files
