cdd22721b49a9cdcd88a77cb09730ff86444e07d
ivanharvard <144486839+ivanharvard@users.noreply.github.com>
2025-06-24 16:45:22 -0400

    Added test_out_of_range_large back

--- game/__init__.py
+++ game/__init__.py
@@ -45,6 +45,15 @@ def test_string_guess():
 
 
 @check50.check(test_valid_level)
+def test_out_of_range_large():
+    """game.py rejects guess above specified range with \"Too large!\""""
+    output = "Too large!"
+    check50.run("python3 testing.py").stdin("4", prompt=True).stdin(
+        "8", prompt=True
+    ).stdout(regex(output), output, regex=True).reject()
+    
+
+@check50.check(test_valid_level)
 def test_nonpositive_guess():
     """game.py rejects nonpositive guess"""
     check50.run("python3 game.py").stdin("1", prompt=True).stdin(
