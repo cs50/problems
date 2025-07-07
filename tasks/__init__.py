c3f4c3a826160b6274898f3543c877b27ae2b185
Brandon Nguyen <112731698+bxngyn@users.noreply.github.com>
2025-07-07 10:47:49 -0400

    update testing script

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -9,51 +9,52 @@ def exists():
 
 @check50.check(exists)
 def test_correct_input():
-    """correctly accepts two command-line inputs"""
+    """tasks.py correctly accepts two command-line inputs"""
     check50.run("python3 tasks.py").exit(code=1)
 
 @check50.check(exists)
+def test_simple_add():
+    """tasks.py correctly handles simple add case"""
+    output = "1. Attend Lecture\n2. Rip a Phonebook\n3. Eat"
+    check50.run("python3 tasks.py monday.txt").stdin("add Eat", prompt=True).stdout(regex(output), output).kill()
+
+@check50.check(exists)
 def test_print_add():
-    """correctly prints added task"""
+    """tasks.py correctly prints added task"""
     output = "1. Attend Lecture\n2. Rip a Phonebook\n3. Call David"
     check50.run("python3 tasks.py monday.txt").stdin("add Call David", prompt=True).stdout(regex(output), output).kill()
 
 @check50.check(exists)
 def test_case_add():
-    """disregards the case of add"""
+    """tasks.py correctly handles case-sensitive add"""
     output = "1. Attend Lecture\n2. Rip a Phonebook\n3. Call David"
     check50.run("python3 tasks.py monday.txt").stdin("aDd Call David", prompt=True).stdout(regex(output), output).kill()
 
 @check50.check(exists)
-def test_add_txt():
-    """test"""
-    check50.run("python3 testing.py get_level")
-
-@check50.check(exists)
 def test_print_remove():
-    """correctly prints list after removal"""
+    """tasks.py correctly prints list after removal"""
     output = "1. Rip a Phonebook"
     check50.run("python3 tasks.py monday.txt").stdin("remove Attend Lecture", prompt=True).stdout(regex(output), output).kill()
 
 @check50.check(exists)
 def test_case_remove():
-    """disregards the case of remove"""
+    """tasks.py correctly handles case-sensitive remove"""
     output = "1. Rip a Phonebook"
     check50.run("python3 tasks.py monday.txt").stdin("rEMoVe Attend Lecture", prompt=True).stdout(regex(output), output).kill()
 
 @check50.check(exists)
 def test_invalid_remove():
-    """correctly exits when no match to remove"""
+    """tasks.py correctly exits when no match to remove"""
     check50.run("python3 tasks.py monday.txt").stdin("remove Feed the Cat", prompt=True).exit(code=1)
 
 @check50.check(exists)
 def test_just_command():
-    """correctly exits when no description"""
+    """tasks.py correctly exits when no task is given"""
     check50.run("python3 tasks.py monday.txt").stdin("add", prompt=True).exit(code=1)
 
 @check50.check(exists)
 def test_invalid_command():
-    """correctly exits when invalid command"""
+    """tasks.py correctly exits when invalid operation"""
     check50.run("python3 tasks.py monday.txt").stdin("edit Rip a Phonebook", prompt=True).exit(code=1)
 
 def regex(text):
