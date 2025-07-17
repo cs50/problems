2f8f903aafc0c6ad46e8239608c3c28035b5bb38
Rongxin Liu <rongxinliu.dev@gmail.com>
2025-07-17 16:49:23 -0400

    replace lambda with list comprehension for ONE_POINT_LETTERS and update stdout arguments in tests

--- scrabble/__init__.py
+++ scrabble/__init__.py
@@ -6,7 +6,7 @@ import random
 POINTS = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
 # Generate a list of letters that have one point in Scrabble
-ONE_POINT_LETTERS = lambda: [chr(i + ord('a')) for i in range(26) if POINTS[i] == 1]
+ONE_POINT_LETTERS = [chr(i + ord('a')) for i in range(26) if POINTS[i] == 1]
 
 # Create a mapping of letters to their Scrabble points
 POINTS_TABLE = {chr(i + ord('a')): POINTS[i] for i in range(26)}
@@ -42,47 +42,47 @@ def compiles():
 @check50.check(compiles)
 def tie_letter_case():
     """handles letter cases correctly"""
-    check50.run("./scrabble").stdin("LETTERCASE").stdin("lettercase").stdout("[Tt]ie!?", "Tie!").exit(0)
+    check50.run("./scrabble").stdin("LETTERCASE").stdin("lettercase").stdout(*TIE).exit(0)
 
 @check50.check(compiles)
 def tie_punctuation():
     """handles punctuation correctly"""
-    check50.run("./scrabble").stdin("Punctuation!?!?").stdin("punctuation").stdout("[Tt]ie!?", "Tie!").exit(0)
+    check50.run("./scrabble").stdin("Punctuation!?!?").stdin("punctuation").stdout(*TIE).exit(0)
 
 @check50.check(compiles)
 def test1():
     """correctly identifies 'Question?' and 'Question!' as a tie"""
-    check50.run("./scrabble").stdin("Question?").stdin("Question!").stdout("[Tt]ie!?", "Tie!").exit(0)
+    check50.run("./scrabble").stdin("Question?").stdin("Question!").stdout(*TIE).exit(0)
 
 @check50.check(compiles)
 def test2():
     """correctly identifies 'drawing' and 'illustration' as a tie"""
-    check50.run("./scrabble").stdin("drawing").stdin("illustration").stdout("[Tt]ie!?", "Tie!").exit(0)
+    check50.run("./scrabble").stdin("drawing").stdin("illustration").stdout(*TIE).exit(0)
 
 @check50.check(compiles)
 def test3():
     """correctly identifies 'hai!' as winner over 'Oh,'"""
-    check50.run("./scrabble").stdin("Oh,").stdin("hai!").stdout("[Pp]layer 2 [Ww]ins!?", "Player 2 wins!").exit(0)
+    check50.run("./scrabble").stdin("Oh,").stdin("hai!").stdout(*PLAYER_2_WINS).exit(0)
 
 @check50.check(compiles)
 def test4():
     """correctly identifies 'COMPUTER' as winner over 'science'"""
-    check50.run("./scrabble").stdin("COMPUTER").stdin("science").stdout("[Pp]layer 1 [Ww]ins!?", "Player 1 wins!").exit(0)
+    check50.run("./scrabble").stdin("COMPUTER").stdin("science").stdout(*PLAYER_1_WINS).exit(0)
 
 @check50.check(compiles)
 def test5():
     """correctly identifies 'Scrabble' as winner over 'wiNNeR'"""
-    check50.run("./scrabble").stdin("Scrabble").stdin("wiNNeR").stdout("[Pp]layer 1 [Ww]ins!?", "Player 1 wins!").exit(0)
+    check50.run("./scrabble").stdin("Scrabble").stdin("wiNNeR").stdout(*PLAYER_1_WINS).exit(0)
 
 @check50.check(compiles)
 def test6():
     """correctly identifies 'pig' as winner over 'dog'"""
-    check50.run("./scrabble").stdin("pig").stdin("dog").stdout("[Pp]layer 1 [Ww]ins!?", "Player 1 wins!").exit(0)
+    check50.run("./scrabble").stdin("pig").stdin("dog").stdout(*PLAYER_1_WINS).exit(0)
 
 @check50.check(compiles)
 def complex_case():
     """correctly identifies 'Skating!' as winner over 'figure?'"""
-    check50.run("./scrabble").stdin("figure?").stdin("Skating!").stdout("[Pp]layer 2 [Ww]ins!?", "Player 2 wins!").exit(0)
+    check50.run("./scrabble").stdin("figure?").stdin("Skating!").stdout(*PLAYER_2_WINS).exit(0)
 
 @check50.check(complex_case)
 def test_strict_order():
