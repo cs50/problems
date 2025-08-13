c49d437c2a9833108aad1bccf8247c407cd1e49e
Rongxin Liu <rongxinliu.dev@gmail.com>
2025-08-13 03:00:08 +0100

    refactor credit card tests to use helper function

--- credit/__init__.py
+++ credit/__init__.py
@@ -11,77 +11,84 @@ def compiles():
     """credit.c compiles"""
     check50.c.compile("credit.c", lcs50=True)
 
+def _test_credit_card(card_number, expected_output):
+    """Helper function to test credit card validation with proper error handling"""
+    try:
+        check50.run("./credit").stdin(card_number).stdout(expected_output).stdout(check50.EOF).exit(0)
+    except check50.Missing as e:
+        raise check50.Failure("Program did not exit after processing input.")
+
 @check50.check(compiles)
 def test1():
     """identifies 378282246310005 as AMEX"""
-    check50.run("./credit").stdin("378282246310005").stdout("AMEX\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("378282246310005", "AMEX\n")
 
 @check50.check(compiles)
 def test2():
     """identifies 371449635398431 as AMEX"""
-    check50.run("./credit").stdin("371449635398431").stdout("AMEX\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("371449635398431", "AMEX\n")
 
 @check50.check(compiles)
 def test3():
     """identifies 5555555555554444 as MASTERCARD"""
-    check50.run("./credit").stdin("5555555555554444").stdout("MASTERCARD\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("5555555555554444", "MASTERCARD\n")
 
 @check50.check(compiles)
 def test4():
     """identifies 5105105105105100 as MASTERCARD"""
-    check50.run("./credit").stdin("5105105105105100").stdout("MASTERCARD\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("5105105105105100", "MASTERCARD\n")
 
 @check50.check(compiles)
 def test5():
     """identifies 4111111111111111 as VISA"""
-    check50.run("./credit").stdin("4111111111111111").stdout("VISA\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("4111111111111111", "VISA\n")
 
 @check50.check(compiles)
 def test6():
     """identifies 4012888888881881 as VISA"""
-    check50.run("./credit").stdin("4012888888881881").stdout("VISA\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("4012888888881881", "VISA\n")
 
 @check50.check(compiles)
 def test7():
     """identifies 4222222222222 as VISA"""
-    check50.run("./credit").stdin("4222222222222").stdout("VISA\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("4222222222222", "VISA\n")
 
 @check50.check(compiles)
 def test8():
     """identifies 1234567890 as INVALID (invalid length, checksum, identifying digits)"""
-    check50.run("./credit").stdin("1234567890").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("1234567890", "INVALID\n")
 
 @check50.check(compiles)
 def test9():
     """identifies 369421438430814 as INVALID (invalid identifying digits)"""
-    check50.run("./credit").stdin("369421438430814").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("369421438430814", "INVALID\n")
 
 @check50.check(compiles)
 def test10():
     """identifies 4062901840 as INVALID (invalid length)"""
-    check50.run("./credit").stdin("4062901840").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("4062901840", "INVALID\n")
 
 @check50.check(compiles)
 def test11():
     """identifies 5673598276138003 as INVALID (invalid identifying digits)"""
-    check50.run("./credit").stdin("5673598276138003").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("5673598276138003", "INVALID\n")
 
 @check50.check(compiles)
 def test12():
     """identifies 4111111111111113 as INVALID (invalid checksum)"""
-    check50.run("./credit").stdin("4111111111111113").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("4111111111111113", "INVALID\n")
 
 @check50.check(compiles)
 def test13():
     """identifies 4222222222223 as INVALID (invalid checksum)"""
-    check50.run("./credit").stdin("4222222222223").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("4222222222223", "INVALID\n")
 
 @check50.check(compiles)
 def test14():
     """identifies 3400000000000620 as INVALID (AMEX identifying digits, VISA/Mastercard length)"""
-    check50.run("./credit").stdin("3400000000000620").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("3400000000000620", "INVALID\n")
 
 @check50.check(compiles)
 def test15():
     """identifies 430000000000000 as INVALID (VISA identifying digits, AMEX length)"""
-    check50.run("./credit").stdin("430000000000000").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+    _test_credit_card("430000000000000", "INVALID\n")
